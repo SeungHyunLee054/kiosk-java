fea1f6e6ead60decd9d94d639c7930e3fa6e0cec
SeungHyunLee054 <yellow13lsh@naver.com>
2025-03-13 16:39:17 +0900

    Refactor : Cart의 Map으로 선언한 속성을 Cart에서 생성하지 않고 Cart객체를 생성할 때 주입하도록 변경
    Map 속성이 객체 생성때 한 개만 생성되도록 하기 위함

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,21 @@ import module.io.input.Input;
 import module.io.output.Output;
 import module.kiosk.Kiosk;
 import module.menu.domain.model.Menu;
+import module.menu.domain.model.MenuItem;
 import module.menu.service.MenuService;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Input input = new Input();
         Output output = new Output();
         List<Menu> menuList = new ArrayList<>();
-        Cart cart = new Cart(new HashMap<>());
+        Map<MenuItem, Integer> cartItems = new HashMap<>();
+        Cart cart = new Cart(cartItems);
         CartService cartService = new CartService(input, cart);
         MenuService menuService = new MenuService(menuList);
         Kiosk kiosk = new Kiosk(menuService, input, output, cartService);
